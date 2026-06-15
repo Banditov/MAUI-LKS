f6040190d95d47170284ee095bac71376a9afcad
Banditov <vito20samsung21@gmail.com>
2026-06-15 14:39:02 +0700

    feat: Created register page

--- MAUI LKS2/Pages/Login.xaml.cs
+++ MAUI LKS2/Pages/Login.xaml.cs
@@ -5,21 +5,35 @@ public partial class Login : ContentPage
 	public Login()
 	{
 		InitializeComponent();
+
+		EmailEntry.TextChanged += OnFormChange;
+		PasswordEntry.TextChanged += OnFormChange;
 	}
+
+	private void OnFormChange (object? sender, EventArgs e)
+	{
+		bool isActive = !string.IsNullOrEmpty(EmailEntry.Text) &&
+						!string.IsNullOrEmpty(PasswordEntry.Text);
+
+		LoginBtn.IsEnabled = isActive;
+    }
 
 	private void OnLoginClicked(object? sender, EventArgs e)
 	{
-
-	}
+        
+    }
 
 	private void OnEyeBtnClicked(object? sender, EventArgs e)
 	{
         if (PasswordEntry.IsPassword == true)
 		{
 			PasswordEntry.IsPassword = false;
-        } else
+			PasswordShow.BackgroundColor = Color.FromArgb("#464646");
+        }
+		else
 		{
 			PasswordEntry.IsPassword = true;
+			PasswordShow.BackgroundColor = Color.FromArgb("#7676ED");
 		}
 	}
 
--- MAUI LKS2/Pages/Register.xaml.cs
+++ MAUI LKS2/Pages/Register.xaml.cs
@@ -5,5 +5,50 @@ public partial class Register : ContentPage
 	public Register()
 	{
 		InitializeComponent();
+
+        PasswordEntry.TextChanged += OnFormInput;
+        PasswordEntryConf.TextChanged += OnFormInput;
+        EmailEntry.TextChanged += OnFormInput;
 	}
+
+    private void OnFormInput(object? sender, EventArgs e)
+    {
+        bool isActive = !string.IsNullOrEmpty(EmailEntry.Text) &&
+                        !string.IsNullOrEmpty(PasswordEntry.Text) &&
+                        !string.IsNullOrEmpty(PasswordEntryConf.Text);
+
+        RegisterBtn.IsEnabled = isActive;
+    }
+
+    private async void OnLoginBtnClicked (object? sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("Pages/Login");
+	}
+
+	private void OnEyeBtnClicked(object? sender, EventArgs e)
+	{
+		if (PasswordEntry.IsPassword == true)
+		{
+			PasswordEntry.IsPassword = false;
+            PasswordShow.BackgroundColor = Color.FromArgb("#464646");
+        }
+		else
+		{
+			PasswordEntry.IsPassword = true;
+            PasswordShow.BackgroundColor = Color.FromArgb("#7676ED");
+        }
+	}
+    private void OnEyeBtnClickedConf(object? sender, EventArgs e)
+    {
+        if (PasswordEntryConf.IsPassword == true)
+        {
+            PasswordEntryConf.IsPassword = false;
+            PasswordShow.BackgroundColor = Color.FromArgb("#464646");
+        }
+        else
+        {
+            PasswordEntryConf.IsPassword = true;
+            PasswordShow.BackgroundColor = Color.FromArgb("#7676ED");
+        }
+    }
 }
